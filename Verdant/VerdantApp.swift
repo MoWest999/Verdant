d29cbfffb9f4403e7cea0301a27f0c614287b2dc
Monique West <asl_mowest@yahoo.com>
2023-02-03 09:21:27 -0500

    Initial Commit

---
+++ Verdant/VerdantApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerdantApp.swift
+//  Verdant
+//
+//  Created by Monique West on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct VerdantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
